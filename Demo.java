bc2ef02ea6a3073ebd8da09cef8f16883a467144
Ramees Raj <rameesraj534@gmail.com>
2023-09-07 16:28:39 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class Demo {
+  public static void main(String[] args) {
+    // Create a new scanner object
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = myObj.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
+  }         
+}            
+                 
+         
+          
+          
+           
