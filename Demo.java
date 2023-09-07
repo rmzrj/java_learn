c586420f33495c4bab7161d86b4b4a6c2c4c5686
Ramees Raj <rameesraj534@gmail.com>
2023-09-07 17:21:31 +0530

    comparator vs comparable

--- Demo.java
+++ Demo.java
@@ -1,16 +1,58 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Students implements Comparable<Students>{
+  int age;
+  String name;
+
+  public Students(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
+
+  public String toString() {
+    return "Students [age=" + age + ", name=" + name + "]";
+  }
+
+  @Override
+  public int compareTo(Students that) {
+ 
+        if(this.age > that.age)
+        return 1;
+        else
+        return -1;
+    
+  }
+
+}
 
 public class Demo {
-  public static void main(String[] args) {
-    Collection<Integer> nums = new ArrayList<Integer>();
-    nums.add(5);
-    nums.add(3);
-    nums.add(19);
+  public static void main(String a[]) {
+   
+    Comparator<Students> com = new Comparator<Students>() {
+      public int compare(Students i , Students j){
+        if(i.age > j.age)
+        return 1;
+        else
+        return -1;
+      }
+    };
 
-  for( int n  : nums){
-    System.out.println(n);
-  }
+    List<Students> stdnts = new ArrayList<>();
+    stdnts.add(new Students(12, "EMI"));
+         stdnts.add(new Students(20, "Labee"));
+    stdnts.add(new Students(13, "Ramees"));
+
+
+     Collections.sort(stdnts);
+
+     for( Students s : stdnts){
+         System.out.println("Name: "+s.name+" Age:"+s.age);
+     }
+
 }
 }            
                  
